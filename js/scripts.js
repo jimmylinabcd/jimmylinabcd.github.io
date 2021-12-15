94e5b6aa7a89985b433040a2b5b149e8a6f3823e
jimmylinabcd <jimmylinabcd@gmail.com>
2021-12-15 16:40:14 +1000

    removed portfolio.html and replace with section on the main page

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 console.log("Hi! Unfortunately there is nothing to find back here! Bye! ")
+$("#portfolio-content").hide();
 //Cursor
 let circle = document.getElementById('circle');
 const onMouseMove = (e) =>{
@@ -6,3 +7,23 @@ const onMouseMove = (e) =>{
   circle.style.top = e.pageY + 'px';
 }
 document.addEventListener('mousemove', onMouseMove);
+
+//switching
+var fadeSpeed = 300;
+document.getElementById("home").addEventListener("click", function(){
+  setTimeout(function(){
+    $("#home-content").fadeIn(fadeSpeed);
+  }, fadeSpeed);
+  $("#portfolio-content").fadeOut(fadeSpeed);
+  
+})
+
+document.getElementById("portfolio").addEventListener("click", function(){
+  setTimeout(function(){
+    $("#portfolio-content").fadeIn(fadeSpeed);
+  }, fadeSpeed);
+  $("#home-content").fadeOut(fadeSpeed);
+})
+
+
+
